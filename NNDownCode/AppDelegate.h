df364e395f9b42c07d93e2de497a3146267a1dd1
wax2045 <861412028@qq.com>
2018-03-01 17:25:39 +0800

    Initial Commit

---
+++ NNDownCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NNDownCode
+//
+//  Created by FUWANG on 2018/3/1.
+//  Copyright © 2018年 Shenzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NNDownCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NNDownCode
+//
+//  Created by FUWANG on 2018/3/1.
+//  Copyright © 2018年 Shenzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
